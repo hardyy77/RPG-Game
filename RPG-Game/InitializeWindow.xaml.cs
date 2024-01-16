df91fb66f6fa962e3d151d944c0d3ae2470ad6e4
hardyy77 <arek.lorenc96@gmail.com>
2024-01-16 16:00:48 +0100

    Added something
    Added player statistics and welcome window

---
+++ RPG-Game/InitializeWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RPG_Game
+{
+    public partial class InitialWindow : Window
+    {
+        public InitialWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGameClick(object sender, RoutedEventArgs e)
+        {
+            string playerName = PlayerNameTextBox.Text;
+
+            // Otwórz nowe okno i przekaż nazwę gracza
+            MainWindow mainWindow = new MainWindow(playerName);
+            mainWindow.Show();
+
+            // Zamknij bieżące okno (InitialWindow)
+            Close();
+        }
+    }
+}
